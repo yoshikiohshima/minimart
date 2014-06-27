584831e3e905915d9c4454ee13618eb2507f03e3
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-06-27 00:36:19 -0400

    Note re: #:arguments

--- minimart/actor.rkt
+++ minimart/actor.rkt
@@ -3,6 +3,9 @@
 ;; TODO: syntax-id-rules: raise-syntax-error on set!, pointing users
 ;; to the #:update pseudo-action.
 
+;; TODO: enforce presence of #:arguments, and enforce that it declares
+;; all the free variables in the actor.
+
 (provide actor
 	 observe-subscribers
 	 observe-advertisers
